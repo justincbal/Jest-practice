f102eeb70e488f9b8e3ad06f4ad44f83f09f8789
Justin Balagtas <justinseebee@hotmail.com>
2023-06-28 17:39:35 -0400

    complete tests 1 - 4

--- code.js
+++ code.js
@@ -18,4 +18,25 @@ let Calculator = {
     divide: function(a,b) { return a / b}
 }
 
-module.exports = {capitalize, reverseString, Calculator};
+function shift(str, num) {
+    let letters = 'abcdefghijklmnopqrstuvwxyz';
+    let newStr = "";
+    for(let i = 0; i < str.length; i++) {
+        let letterIndex = letters.indexOf(str[i]);
+        
+        if(letterIndex + num > 25) {
+            /*
+                [23]x + 5 = c
+            */
+           let diff = 25 - letterIndex; // 2
+           newStr += letters[-1 + (num - diff)];
+        }
+        else {
+            newStr += letters[letterIndex + num];
+        }
+        
+    }
+    return newStr;
+}
+
+module.exports = {capitalize, reverseString, Calculator, shift};
--- code.test.js
+++ code.test.js
@@ -1,4 +1,4 @@
-const {capitalize, reverseString, Calculator} = require('./code');
+const {capitalize, reverseString, Calculator, shift} = require('./code');
 
 // Capitalize: Take a string and return it with first letter capitalized hello => Hello
 test('Return a string capitalized', () => {
@@ -18,3 +18,10 @@ test('Calculator', () => {
     expect(Calculator.divide(72,24)).toBe(3);
 })
 
+// Caesar Cipher: Shifts each character by a given amount | "abcdef" => bcdefg shifted 1
+test('Shift characters by set amount', () => {
+    expect(shift('abc', 1)).toMatch(/bcd/);
+    expect(shift('justin', 3)).toMatch(/mxvwlq/);
+    expect(shift('xyz', 5)).toMatch(/cde/);
+    expect(shift('justin', 10)).toMatch(/tecdsx/);
+})
