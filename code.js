5b172deb6e1884327c4d765948f5633024d4caff
Justin Balagtas <justinseebee@hotmail.com>
2023-06-28 16:32:04 -0400

    complete test 1, 2, 3

--- code.js
+++ code.js
@@ -2,4 +2,20 @@ function capitalize(str) {
     return str[0].toUpperCase() + str.slice(1);
 }
 
-module.exports = capitalize;
+function reverseString(str) {
+    let newStr = "";
+    for(let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+
+    return newStr;
+}
+
+let Calculator = {
+    add: function(a,b) { return a + b; },
+    subtract: function(a,b) { return a - b},
+    multiply: function(a,b) { return a * b},
+    divide: function(a,b) { return a / b}
+}
+
+module.exports = {capitalize, reverseString, Calculator};
--- code.test.js
+++ code.test.js
@@ -1,6 +1,20 @@
-const capitalize = require('./code');
+const {capitalize, reverseString, Calculator} = require('./code');
 
 // Capitalize: Take a string and return it with first letter capitalized hello => Hello
 test('Return a string capitalized', () => {
     expect(capitalize('hello')).toMatch(/Hello/);
 })
+
+// Reverse string: Take a string and reverse it "Justin" -> "nitsuJ";
+test('Reverse a string', () => {
+    expect(reverseString('Justin')).toMatch(/nitsuJ/);
+})
+
+// Calculator: Functioning calculator object that adds, subtracts, multiplies, and divides
+test('Calculator', () => {
+    expect(Calculator.add(5,3)).toBe(8);
+    expect(Calculator.subtract(10,4)).toBe(6);
+    expect(Calculator.multiply(6,6)).toBe(36);
+    expect(Calculator.divide(72,24)).toBe(3);
+})
+
